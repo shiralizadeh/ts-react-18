473322feb6da6b683abd349177372305785870d3
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2023-09-23 22:03:43 +0200

    Without lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React, { Suspense } from "react";
-
-const LazyComponent = React.lazy(() => import("./components/LazyComponent"));
+import React from "react";
+import LazyComponent from "./components/LazyComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>Home Page</h1>
-      <Suspense fallback={<div>Loading...</div>}>
-        <LazyComponent />
-      </Suspense>
+      <LazyComponent />
     </div>
   );
 }
