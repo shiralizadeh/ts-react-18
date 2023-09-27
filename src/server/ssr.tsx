06418020e4b9794194ebeb75ddf51902e6fd25af
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2023-09-27 20:42:33 +0200

    Modify server

--- src/server/ssr.tsx
+++ src/server/ssr.tsx
@@ -1,18 +1,7 @@
-import express from "express";
 import React from "react";
-import { renderToStaticNodeStream } from "react-dom/server";
+import { renderToString } from "react-dom/server";
 import { App } from "../App";
 
-const app = express();
+const html = renderToString(<App />);
 
-app.get("/", (req: any, res: any) => {
-  const stream = renderToStaticNodeStream(<App />);
-
-  res.setHeader("Content-Type", "text/html");
-
-  stream.pipe(res);
-});
-
-app.listen(4000, () => {
-  console.log("Server is listening on port: 4000");
-});
+console.log(html);
